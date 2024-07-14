a1eaffcda085d5bc574b1074564eb199dddc4ce6
tlochsta <81081702+tlochsta@users.noreply.github.com>
2024-07-14 16:12:17 -0400

    Create games.js

---
+++ src/games.js
@@ -0,0 +1,6 @@
+  const games = [
+    { id: 1, title: "Game 1", url: "https://example.com/game1", image: "https://via.placeholder.com/400x400" },
+    { id: 2, title: "Game 2", url: "https://example.com/game2", image: "https://via.placeholder.com/400x400" },
+    { id: 3, title: "Game 3", url: "https://example.com/game3", image: "https://via.placeholder.com/400x400" },
+    { id: 4, title: "Game 4", url: "https://example.com/game4", image: "https://via.placeholder.com/400x400" },
+  ];
