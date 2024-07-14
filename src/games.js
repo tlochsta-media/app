e88306f4099909e3e7d5347c540384320c996076
tlochsta <81081702+tlochsta@users.noreply.github.com>
2024-07-14 16:30:10 -0400

    Update games.js

--- src/games.js
+++ src/games.js
@@ -1,5 +1,5 @@
   const games = [
-    { id: 1, title: "Game 1", url: "https://example.com/game1", image: "https://via.placeholder.com/400x400" },
+    { id: 1, title: "Tall Man Run", url: "https://8413946450207224144.playables.usercontent.goog/v/assets/index.html", image: "/img/tall-man-run.webp" },
     { id: 2, title: "Game 2", url: "https://example.com/game2", image: "https://via.placeholder.com/400x400" },
     { id: 3, title: "Game 3", url: "https://example.com/game3", image: "https://via.placeholder.com/400x400" },
     { id: 4, title: "Game 4", url: "https://example.com/game4", image: "https://via.placeholder.com/400x400" },
