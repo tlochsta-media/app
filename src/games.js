511977d45650061b91b88120f7372de8faed11ab
tlochsta <81081702+tlochsta@users.noreply.github.com>
2024-07-17 13:31:25 -0400

    Update games.js

--- src/games.js
+++ src/games.js
@@ -7,5 +7,8 @@ const games = [
     { id: 6, title: "MotoX3M Spooky", url: "https://tm-motox3m-spooky.pages.dev", image: "/img/motox3mspooky.jpg", type: "Racing" },
     { id: 7, title: "Bacon May Die", url: "https://tm-bacon-may-die.pages.dev", image: "/img/bacon-may-die.jpg", type: "Survival" },
     { id: 8, title: "Minecraft 1.8", url: "https://tm-eagler18.pages.dev", image: "/img/minecraft18.avif", type: "Sandbox" },
-    { id: 8, title: "Minecraft 1.5", url: "https://astronetclient.pages.dev", image: "/img/minecraft15.jpg", type: "Sandbox" },
+    { id: 9, title: "Minecraft 1.5", url: "https://astronetclient.pages.dev", image: "/img/minecraft15.jpg", type: "Sandbox" },
+    { id: 10, title: "Monkey Mart", url: "https://monkey-mart.pages.dev", image: "/img/monkeymart.jpg", type: "Simulation" },
+    { id: 11, title: "Run 3", url: "https://another-run-3-test-tm.pages.dev/tn6pS9dCf37xAhkJv/", image: "/img/run3.jpg", type: "Endless" },
+    { id: 11, title: "Drift Hunters", url: "https://drift-hunters.pages.dev", image: "/img/drift-hunters.jpg", type: "Racing" },
 ];
