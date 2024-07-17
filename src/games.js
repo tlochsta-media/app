1c675af1b104fc1e2bae2f80a6a2794519712ce6
tlochsta <81081702+tlochsta@users.noreply.github.com>
2024-07-17 13:19:55 -0400

    Update games.js

--- src/games.js
+++ src/games.js
@@ -4,4 +4,6 @@ const games = [
     { id: 3, title: "Rooftop Snipers", url: "https://tm-rooftop-snipers.pages.dev", image: "/img/rooftopsnipers.jpg", type: "Shooter" },
     { id: 4, title: "Fruit Ninja", url: "https://tm-fruit-ninja.pages.dev", image: "/img/fruitninja.webp", type: "Action" },
     { id: 5, title: "Bitlife", url: "https://tm-bitlife.pages.dev", image: "/img/bitlife.jpg", type: "Simulation" },
+    { id: 6, title: "MotoX3M Spooky", url: "https://tm-motox3m-spooky.pages.dev", image: "/img/motox3mspooky.jpg", type: "Racing" },
+    { id: 7, title: "Bacon May Die", url: "https://tm-bacon-may-die.pages.dev", image: "/img/bacon-may-die.jpg", type: "Survival" },
 ];
